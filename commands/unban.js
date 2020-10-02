b0e97738b0f8e78202844541a990455dcf6bccaf
d-bowman159 <64437684+d-bowman159@users.noreply.github.com>
2020-10-02 13:01:50 -0500

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -31,7 +31,9 @@
 
     const channel = message.guild.channels.cache.find(channel => channel.name === "mod-logs")
   
+const banList = await message.guild.fetchBans();
 
+        if(!banList.find(user => user.id === args[0])) return message.reply("User is not banned!")
     //If all steps are completed successfully try kick this user
     message.guild.members.unban(args[0])
         .then(() => console.log(`UnBanned ${User.tag} by ${message.member.displayName}`))
