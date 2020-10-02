48ac2df3ffd81c2053efb468014ac5b2c462f360
d-bowman159 <64437684+d-bowman159@users.noreply.github.com>
2020-10-02 12:32:29 -0500

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -4,11 +4,11 @@
     execute(message, args){
       console.log('unban command sent')
 
-     
+     if(!args[1]) return message.reply('You need to mention a user to unban!')
 
     //Then check if user have permissions to do that
     if(!message.member.hasPermission('BAN_MEMBERS')) {
-        message.channel.send('You must have the permission "Ban Members".');
+        message.reply('You must have the permission "Ban Members".');
         return;
     };
 
@@ -17,13 +17,24 @@
    
 
     
+    const exampleEmbed = new Discord.MessageEmbed()
+    .setColor('#FF0000')
+    .setTitle('Moderation')
+    .setDescription("New Unban!")
+    .addFields(
+        { name: 'Offender', value: `${args[1]}` },
+        { name: "Sender:", value: `${message.member.displayName}` },
+    )
+    .setTimestamp();
 
+    const channel = message.guild.channels.cache.find(channel => channel.name === "mod-logs")
   
 
     //If all steps are completed successfully try kick this user
-    message.guild.members.unban(args[0])
-        .then(() => console.log(`UnBanned ${args[0]} by ${message.member.displayName}`))
-        message.channel.send(`Sucessfully UnBanned ${args[0]}`)
+    message.guild.members.unban(args[1])
+        .then(() => console.log(`UnBanned ${args[1]} by ${message.member.displayName}`))
+        message.channel.send(`Sucessfully UnBanned ${args[1]}`)
+        channel.send(exampleEmbed)
         .catch(console.error);    
     }
 }
