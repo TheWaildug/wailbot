b0423afb7d692cfacdde0b11853d0aaaa555c24e
d-bowman159 <64437684+d-bowman159@users.noreply.github.com>
2020-10-03 11:08:48 -0500

    Update banmessage.js

--- commands/banmessage.js
+++ commands/banmessage.js
@@ -3,6 +3,11 @@ module.exports = {
     name: 'banmessage',
     description: 'changes ban message',
     execute(message,Discord){
+         //Then check if user have permissions to do that
+    if(!message.member.hasPermission('BAN_MEMBERS')) {
+        message.reply('You have no permissions to do that');
+        return;
+    };
         console.log(`changing ban msg`)
         message.reply("See or Change?")
         const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
@@ -14,6 +19,7 @@ module.exports = {
                msg.reply('Please reply with the new ban message.')
                const collector2 = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
                collector2.on('collect',newmsg =>{
+                   print('teqrewewtw')
                     if(!newmsg.content === "Reset"){
     
                         newmsg.reply(`You entered ${newmsg.content} is this correct?`)
