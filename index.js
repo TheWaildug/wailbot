f5159c180e810fb2b3c8c6ba2f87385e02bf4da5
d-bowman159 <64437684+d-bowman159@users.noreply.github.com>
2020-10-03 10:41:41 -0500

    mute

--- index.js
+++ index.js
@@ -58,37 +58,7 @@ client.on('message', message =>{
         client.Commands.get('banmessage').execute(message,Discord)
     }
     else if(command === 'mute'){
-        console.log(`mute command sent.`)
-    if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply('You do not have permission.')
-        const mentionmember = message.mentions.members.first();
-
-        if(!mentionmember)return message.reply('You need to mention a member to mute!') ;
-        console.log(mentionmember.displayName)  
-        
-        if(!mentionmember.kickable) return message.reply('This user cannot be muted!')
-        const muterole = message.guild.roles.cache.find(role => role.name === "Muted");
-        if(!muterole) return message.reply("I couldn't find the mute role!");
-        
-        if(!args[1]) return message.reply('Please have a reason!')
-        const time = args[2]
-        if(!time) return message.reply('Please specify a time!');
-        if(mentionmember.roles.cache.some(role => role.name === 'Muted')) return message.reply("This user is already muted!")
-        mentionmember.roles.add(muterole)
-        .then(() =>  console.log(`Muted ${mentionmember.displayName}  for ${ms(ms(time))} by ${message.member.displayName} Reason: ${args[1]}`)) 
-        message.channel.send(`Sucessfully muted ${mentionmember.displayName} for ${ms(ms(time))}. Reason: ${args[1]}`)
-        mentionmember.send(`You have been muted in ${message.guild.name} for ${ms(ms(time))}. Reason: ${args[1]}`)
-        const channel = message.guild.channels.cache.find(channel => channel.name === "mod-logs")
-        if(channel){channel.send(`${mentionmember.displayName} has been muted by ${message.member.displayName} for ${time}. Reason: ${args[1]}`)}
-
-        
-           
-
-        setTimeout(function(){
-            mentionmember.roles.remove(muterole);
-            if(channel){channel.send(`Auto Unmuted ${mentionmember.displayName}`)};
-            console.log(`Auto Unmuted ${mentionmember.displayName}`)
-            
-        }, ms(time));
+       client.Commands.get('mute').execute(message,args,Discord,ms)
     }
     else if(command === 'unmute'){
         console.log('unmute command sent')
