12371d38042a0bc2046ef2bc8de56adee301e237
d-bowman159 <64437684+d-bowman159@users.noreply.github.com>
2020-10-21 12:47:35 -0500

    Create slowmode.js

---
+++ commands/slowmode.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'slowmode',
+    description: 'changes slowmode',
+    execute(message,args,Discord){
+      console.log('slowmode command sent')
+      //Then check if user have permissions to do that
+    if(!message.member.hasPermission('MANAGE_MESSAGES')) {
+        message.reply('You must have the permission `MANAGE_MESSAGES`.');
+        return;
+    };
+    if(!args[1]) return;
+        const channel = message.mentions.channel.first()   
+        if(channel.type === 'text'){
+            channel.setRateLimitPerUser(args[1])
+        }
+    }
+}
