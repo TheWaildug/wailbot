db053816b4c1d5fe54f0e41440f8bb33be0a220c
d-bowman159 <64437684+d-bowman159@users.noreply.github.com>
2020-10-23 10:53:26 -0500

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const {mongopath} = require('./config.json')
+const { mongPpath } = require('./config.json')
 module.exports = async () =>{
     await mongoose.connect(mongopath)
     return mongoose
